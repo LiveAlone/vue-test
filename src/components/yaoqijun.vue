c4c699548fa5846ff07ec5d67c4b3be38c2b7daa
yaoqijun <yaoqijunmail@gmail.com>
2018-07-04 23:45:32 +0800

    add all

---
+++ src/components/yaoqijun.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="yao">
+    {{ message }}
+    <div class="yao">
+      {{ message }}
+    </div>
+    <ul>
+      <li>test</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'yaoqijun',
+  data () {
+    return {
+      message: 'Hello Vue! sgjasgjnasohnjaisnhajsnflijsbahigjnasjnipgashn'
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  margin: 20px;
+  padding: 0;
+}
+</style>
