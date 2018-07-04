2d9fa49242a397d0f4a02223d4b13be3f5dae645
yaoqijun <yaoqijunmail@gmail.com>
2018-07-05 00:01:18 +0800

    add all content test

---
+++ src/components/teacher.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="test">
+      <ul>
+        <li>{{ msg1 }}</li>
+        <li>{{ msg2 }}</li>
+        <li>{{ msg3 }}</li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'teacher',
+  data () {
+    return {
+      msg1: 'yao',
+      msg2: 'qi',
+      msg3: 'jun'
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul, li {
+  margin: 20px;
+  padding: 0;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import yaoqijun from '@/components/yaoqijun'
+import teacher from '@/components/teacher'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/yaoqijun',
       name: 'yaoqijun',
       component: yaoqijun
+    },
+    {
+      path: '/teacher',
+      name: 'teacher',
+      component: teacher
     }
   ]
 })
